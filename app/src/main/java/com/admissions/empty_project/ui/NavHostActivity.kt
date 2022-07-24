f5d1cfcb06c9dbf67a47f19dddcbe87234950d7f
yerih 3d link <yerih@3dlinkweb.com>
2022-07-23 23:03:44 -0400

    app configurada correctamente

--- app/src/main/java/com/admissions/empty_project/ui/NavHostActivity.kt
+++ app/src/main/java/com/admissions/empty_project/ui/NavHostActivity.kt
@@ -3,7 +3,9 @@ package com.admissions.empty_project.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.admissions.empty_project.data.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NavHostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
